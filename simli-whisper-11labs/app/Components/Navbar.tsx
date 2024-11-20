c3b51a97263482155a48f3468d6548ffb9ec323c
TalalBazerbachi <55160650+TalalBazerbachi@users.noreply.github.com>
2024-11-20 18:33:39 +0200

    fix navbar logo

--- simli-whisper-11labs/app/Components/Navbar.tsx
+++ simli-whisper-11labs/app/Components/Navbar.tsx
@@ -2,9 +2,9 @@ import logo from '@/media/bytebeam-black.png';
 import Image from 'next/image';
 export default function Navbar() {
   return (
-    <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 z-50 w-full md:w-auto">
+    <div className="fixed bottom-0 left-0 z-50 w-full md:w-auto">
       <nav>
-        <ul className="font-medium flex p-4 md:p-0 mt-4 border border-black rounded-lg bg-white md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-white md:dark:bg-white">
+        <ul className="font-medium flex p-4 md:p-0 mt-4  rounded-lg bg-white md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-white md:dark:bg-white">
           <li>
             <div className="flex items-center gap-2">
               <span className="text-gray-900 dark:text-balck">Powered By</span>
@@ -17,7 +17,7 @@ export default function Navbar() {
                 <Image
                   src={logo}
                   alt="ByteBeam Logo" 
-                  width={180}
+                  width={120}
                   height={60}
                 />
               </a>
